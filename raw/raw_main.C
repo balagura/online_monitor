e0c76a9e9386ca8cded56f99ffaba080b0a1ec43
balagura <balagura@llr.in2p3.fr>
2015-12-08 17:21:22 +0100

    switched to istream instead of ifstream, added option to skip first N bytes and stop at acqEnd, still, note: skipping does not work for cin

--- raw/raw_main.C
+++ raw/raw_main.C
@@ -1,35 +1,86 @@
 // Author V. Balagura, balagura@cern.ch (19.11.2012)
 
 #include <raw.hh>
-#include <iostream>
+#include <fstream>
 #include <cstdlib>
 #include <string>
 #include <climits> // for INT_MAX
 
+// // taken from http://stackoverflow.com/questions/865668/how-to-parse-command-line-arguments-in-c
+// // to get command line options:
+
+// #include <algorithm>
+// char* getCmdOption(char ** begin, char ** end, const std::string & option) {
+//     char ** itr = std::find(begin, end, option);
+//     if (itr != end && ++itr != end) return *itr;
+//     return 0;
+// }
+// bool cmdOptionExists(char** begin, char** end, const std::string& option) {
+//     return std::find(begin, end, option) != end;
+// }
+// // ------------------------------------------------------------
+
+template<class T> struct DeleteMe { // helper class to ensure that new()'ly created object is deleted in the end
+  DeleteMe() : p(NULL) {}
+  void assign(T* fP) { p = fP; }
+  ~DeleteMe() { if (p != NULL) delete(p); }
+protected:
+  T* p;
+};
+
 int main(int argc, char** argv) {
+  char* filename;
   int nMaxLines = INT_MAX;
   double pedestal_suppression = 0.05;
   string format = "";
-  if (argc < 2 || argc > 5) {
+  streampos skipNBytes = 0;
+  long int acqEnd = 0;
+  if (argc < 2 || argc > 6) {
     cout << "Usage: " << argv[0] << " <file_name> [max_N_lines]\n"
       "or " << argv[0] << " <file_name> max_N_lines high_gain_triggers [pedestal_suppression]\n"
+      "or " << argv[0] << " <file_name> high_gain_triggers pedestal_suppression skipNBytes acqEnd\n\n"
+      "<file_name> can be \'-\' for stdin (note: then skipNBytes can not be used and should be zero)\n"
       "If max_N_lines is absent or 0, all file is processed\n"
       "If pedestal_suppression is given, all not triggered data\n"
-      "is postscaled by this factor (between 0 and 1, default 0.05)\n";
+      "is postscaled by this factor (between 0 and 1, default 0.05)\n"
+      "skipNBytes defines how many first input bytes to skip\n"
+      "acqEnd selects only spill (==acquisition) numbers less than acqEnd (0 to process everything)\n";
     return 0;
   }
   else {
-    if (argc >= 3) {
-      int n = atoi(argv[2]);
-      if (n != 0) nMaxLines = n;
+    filename = argv[1];
+    if (argc < 6) {
+      if (argc >= 3) {
+	int n = atoi(argv[2]);
+	if (n != 0) nMaxLines = n;
+      }
+      if (argc >= 4) format = argv[3];
+      if (argc >= 5) pedestal_suppression = atof(argv[4]);
     }
-    if (argc >= 4) format = argv[3];
-    if (argc >= 5) pedestal_suppression = atof(argv[4]);
+    else {
+      format               = argv[2];
+      pedestal_suppression = atof(argv[3]);
+      skipNBytes           = atoi(argv[4]);
+      acqEnd               = atoi(argv[5]);
+    }
   }
-  ReadSpill spill(argv[1]);
+  ReadSpill spill;
+  DeleteMe<ifstream> closeFileInTheEnd;
+  istream* is;
+  if (string(filename) == string("-"))
+    is = &cin;
+  else {
+    ifstream* file = new(ifstream);
+    closeFileInTheEnd.assign(file);
+    file->open(filename, ios::in | ios::binary);
+    is = file;
+  }
+  if (skipNBytes != 0) is->seekg(skipNBytes);
+  spill.open(*is);
   int iLine=0;
-
+  
   while (spill.next()) {
+    if ( acqEnd != 0 & spill.acquisition_number() >= acqEnd ) return 0;
     const map<unsigned short int, vector<ChipSCA> >& sca = spill.sca();
     for (map<unsigned short int, vector<ChipSCA> >::const_iterator isca=sca.begin(); isca!=sca.end(); ++isca) {
       int bx  =  isca->first;
